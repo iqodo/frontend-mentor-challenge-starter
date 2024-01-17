2ad6d790d2677f635b6de178ac47f7626bca9f61
Marco <m.derover@gmail.com>
2024-01-17 09:57:39 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+  site: 'https://markteekman.github.io',
+  base: '/frontend-mentor-challenge-starter',
+})
